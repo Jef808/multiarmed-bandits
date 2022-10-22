a7dc44fad29e418245de972cc850204c1318d2aa
Jean-Francois Arbour <jf.arbour@gmail.com>
2022-10-21 22:00:52 -0400

    Basic form component

--- web/frontend/src/App.vue
+++ web/frontend/src/App.vue
@@ -1,19 +1,135 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import FormRequest from "./components/FormRequest.vue";
-import SubmitRequest from "./components/SubmitRequest.vue";
+import DataContext from "./components/DataContext.vue";
+import DataView from "./components/DataView.vue";
 </script>
 
 <template>
-  <div class="controls">
-    <FormRequest />
-    <SubmitRequest />
+  <div class="flex container">
+    <div id="data-view" class="flex">
+      <DataView />
+    </div>
+    <div id="data-context" class="flex">
+      <DataContext />
+    </div>
   </div>
 </template>
 
-<style scoped>
-.controls {
-    display: flex;
+<style>
+/* Global styles */
+.btn {
+  padding: 0.8rem 1rem 0.7rem;
+  border: 0.2rem solid #4d4d4d;
+  cursor: pointer;
+  text-transform: capitalize;
 }
+.btn__danger {
+  color: #fff;
+  background-color: #ca3c3c;
+  border-color: #bd2130;
+}
+.btn__filter {
+  border-color: lightgrey;
+}
+.btn__danger:focus {
+  outline-color: #c82333;
+}
+.btn__primary {
+  color: #fff;
+  background-color: #000;
+}
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+}
+.btn-group > * {
+  flex: 1 1 auto;
+}
+.btn-group > * + * {
+  margin-left: 0.8rem;
+}
+.label-wrapper {
+  margin: 0;
+  flex: 0 0 100%;
+  text-align: center;
+}
+[class*="__lg"] {
+  display: inline-block;
+  width: 100%;
+  font-size: 1.9rem;
+}
+[class*="__lg"]:not(:last-child) {
+  margin-bottom: 1rem;
+}
+@media screen and (min-width: 620px) {
+  [class*="__lg"] {
+    font-size: 2.4rem;
+  }
+}
+.visually-hidden {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap;
+}
+[class*="stack"] > * {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.stack-small > * + * {
+  margin-top: 1.25rem;
+}
+.stack-large > * + * {
+  margin-top: 2.5rem;
+}
+@media screen and (min-width: 550px) {
+  .stack-small > * + * {
+    margin-top: 1.4rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.8rem;
+  }
+}
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 2rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+}
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+#app > form {
+  max-width: 100%;
+}
+#app h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 1rem;
+}
+.flex container {
+  flex-flow: row nowrap;
+}
+#data-view {
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+}
+#data-context {
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+}
+
 </style>
--- web/frontend/src/components/DataView.vue
+++ web/frontend/src/components/DataView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, PropType } from "vue";
+import { computed } from "vue";
 import { Bar } from "vue-chartjs";
 import {
   Chart as ChartJS,
@@ -9,7 +9,6 @@ import {
   BarElement,
   CategoryScale,
   LinearScale,
-  Plugin,
 } from "chart.js";
 
 ChartJS.register(
@@ -21,74 +20,42 @@ ChartJS.register(
   LinearScale
 );
 
-defineProps({
-  chartId: {
-    type: String,
-    default: "bar-chart",
-  },
-  width: {
-    type: Number,
-    default: 400,
-  },
-  height: {
-    type: Number,
-    default: 400,
-  },
-  cssClasses: {
-    type: String,
-    default: "",
-  },
-  styles: {
-    type: Object as PropType<Partial<CSSStyleDeclaration>>,
-    default: () => {},
-  },
-  plugins: {
-    type: Array as PropType<Plugin<"bar">[]>,
-    default: () => [],
-  },
+const chartLabels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const chartData = computed(() => {
+  return {
+    labels: chartLabels,
+    datasets: [
+      {
+        label: "Data One",
+        backgroundColor: "#f87979",
+        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+      },
+    ],
+  };
 });
 
-const chartOptions = {
-  responsive: true,
-  maintainAspectRatio: false,
-};
-
-const chartData = ref({
-  labels: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ],
-  datasets: [
-    {
-      label: "Data One",
-      backgroundColor: "#f87979",
-      data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
-    },
-  ],
+const chartOptions = computed(() => {
+  return {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
 });
 </script>
 
 <template>
-  <div>
-      <Bar
-        :chart-data="chartData"
-        :chart-options="chartOptions"
-        :chart-id="chartId"
-        :width="width"
-        :height="height"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :plugins="plugins"
-      />
-  </div>
+  <Bar :chart-data="chartData" :chart-options="chartOptions" />
 </template>
